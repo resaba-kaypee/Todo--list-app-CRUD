178c1d3d2410458b24dbb6af7636db4e43ba4dd9
kaypee <resaba.kaypee@gmail.com>
2019-06-26 20:02:49 +0700

    init db connection

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+const path = require('path');
+
+const db = require('./db');
+const collection = 'todo';
+
+db.connect(err => {
+  if(err) {
+    console.log(`Unable to connect to database`);
+    process.exit(1)
+  } else {
+    app.listen(3000, () => {
+      console.log(`Connected to database, listening on port 3000`)
+    });
+  }
+})
